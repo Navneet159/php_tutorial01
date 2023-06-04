6d1ea5df8a21df88e0c089b9fde3dd30ae439dfb
Navneet <navneet.prajapat.work@gmail.com>
2023-06-04 12:08:37 +0530

    php tut

---
+++ assignment/strict-comparison.php
@@ -0,0 +1,33 @@
+<?php
+$a=5.67;
+$b=5.67;
+ 
+// if($a==$b){
+//     print("equal hai");
+// }
+
+// if($a!=$b){
+//     print("not eual to");
+// }
+
+// 3 number a,b, &c
+
+$a=10;
+$b=100;
+$c=1000;
+
+
+switch ($a) {
+    case $a<$b : print($a." a is smaller than b");
+        case  $a<$c : print($a." a is smaller than c");
+        break;
+        
+
+    
+    default:
+        # code...
+        break;
+}
+
+
+?>
---
+++ operator.php
@@ -0,0 +1,23 @@
+<?php
+//operator
+//assign operator  3 types
+//= "this operator assign value rigth to left"
+
+//airthmetic operator
+//+= , -=
+
+//other aasigment operator
+// ? :
+
+
+
+//airthmetic operator
+// + , -, /, % ,* 
+
+// comparision operator
+//>,<,>=,<=,==,===,!=,!==
+
+
+
+
+?>
