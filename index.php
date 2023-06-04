d9b5c180420ab7961f54e2b52a6828b6352e96bc
Navneet <navneet.prajapat.work@gmail.com>
2023-06-04 12:11:15 +0530

    y

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li>
+            <a href="./variable.php"> variable</a>
+            <a href="./operator.php"> operator</a>
+        </li>
+    </ul>
+    <li>
+    <h1>assignment</h1>
+        <ul>
+            <li>
+                 <a href="./assignment/switchcase-days.php">switchcase</a>
+                 <a href="./assignment/strict-comparison.php">stric-comparision</a>
+            </li>
+        </ul>
+    </li>
+</body>
+</html>
